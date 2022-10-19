7f07fc9ba2e2d114d4d853c9f08b0f864b796c27
Ramesh Dhungel <RameshDhungel50@gmail.com>
2022-10-18 20:28:20 -0400

    creates the lineup page and adds buttons for each game slate

--- app/src/main/java/com/dfs/dfslineupbuilder/fragment/SlateOptionFragment.java
+++ app/src/main/java/com/dfs/dfslineupbuilder/fragment/SlateOptionFragment.java
@@ -8,58 +8,46 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.dfs.dfslineupbuilder.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SlateOptionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SlateOptionFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SlateOptionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SlateOptionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SlateOptionFragment newInstance(String param1, String param2) {
-        SlateOptionFragment fragment = new SlateOptionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FragmentManager fm = getParentFragmentManager();
-        fm.beginTransaction().add(R.id.GameFragmentContainer, new GameFragment()).commit();
+//        fm.beginTransaction().add(R.id.GameFragmentContainer, new GameFragment()).commit();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slate_option, container, false);
+        View v = inflater.inflate(R.layout.fragment_slate_option, container, false);
+        for(int i = 0; i < 15; i++){
+            addSlate(v,"Slate  "+(i+1));
+        }
+        return v;
+    }
+
+    public void addSlate(View v, String text){
+        LinearLayout layout = v.findViewById(R.id.SlateContainerLayout);
+        Button btn = new Button(getContext());
+        btn.setText(text);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getParentFragmentManager();
+                Bundle bundle = new Bundle();
+                bundle.putString("slate",text+" button");
+                LineUpFragment fragment = new LineUpFragment();
+                fragment.setArguments(bundle);
+                fm.beginTransaction().replace(R.id.ContentFragment,fragment).commit();
+            }
+        });
+        layout.addView(btn);
     }
 }
