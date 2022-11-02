b09e9ab641665173e2044430e641fe66d0ae5885
Riverhawk20 <cadenmrice@gmail.com>
2022-11-02 15:05:48 -0400

    Write User Lambda Function + add SlateId property to returned players in QuerySlate function

---
+++ aws/mobile-dfs-lineup-builder/WriteUserFunction/src/main/java/slate/App.java
@@ -0,0 +1,76 @@
+package slate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.ZonedDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.document.BatchWriteItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.TableWriteItems;
+import com.amazonaws.services.dynamodbv2.model.WriteRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private String DYNAMODB_TABLE_NAME = "dfs-lineup-builder-users";
+    private Regions REGION = Regions.US_EAST_1;
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            User user = new Gson().fromJson(input.getBody(), User.class);
+            AmazonDynamoDBClient clientShell = new AmazonDynamoDBClient();
+            clientShell.setRegion(Region.getRegion(REGION));
+            DynamoDB dbClient = new DynamoDB(clientShell);
+            Table table = dbClient.getTable(DYNAMODB_TABLE_NAME);
+            Item item = new Item()
+                    .withPrimaryKey("UserId", user.UserId)
+                    .withString("Email", user.Email)
+                    .withString("PasswordHash", user.PasswordHash);
+            table.putItem(item);
+            return response
+                    .withStatusCode(200)
+                    .withBody("{status: Success!}");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return response
+                    .withBody("{}")
+                    .withStatusCode(500);
+        }
+    }
+}
